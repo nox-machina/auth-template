cac2dd0989ccf3313dbd67bab9307b0ca38c355a
0xtheMachine <aaronvsummers@gmail.com>
2020-03-20 17:36:37 +0000

    template setup

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require('express');
+var cookieParser = require("cookie-parser");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = process.env.port || 3000
+
+//middleware
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5050"],
+    exposedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"]
+  })
+);
+
+//db connection
+mongoose.connect(`${process.env.DATABASE}`, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log("connected");
+})
+
+app.use(express.json());
+
+//--------------------ROUTES--------------------//
+
+
+app.listen(port, () => console.log(`Example app deployed on port ${port}!`))
